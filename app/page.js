211a4b2bc13abae86b228e2c7e1965a9c6b3929e
Hemant Raj <coder.hemantraj05@gmail.com>
2025-04-12 23:06:28 +0530

    Add next-themes package and update layout and page components

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Header from "./_components/Header";
 
 export default function Home() {
   return (
     <div>
-      <h2>Hello Laudo</h2>
-      <Button>Click me laude</Button>
+      {/* header */}
+      <Header />
+      {/* hero section */}
     </div>
   );
 }
